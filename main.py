9fa9d76c81a575b8ca17dbc20a4212a4a5a6fb18
Ilya Posov <iposov@gmail.com>
2022-03-04 11:16:52 +0300

    решил задачу 2

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-# more changes
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Py Charm')
+    print_hi('!!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
